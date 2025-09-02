3f3030c067b5fb0f47a8dae6e28e81c0040b02fd
Min Maung <minmaung0307@gmail.com>
2025-09-02 18:29:00 -0400

    updated css

--- public/firebase.js
+++ public/firebase.js
@@ -1,9 +1,2 @@
-export const firebaseConfig={apiKey: "AIzaSyBEkph2jnubq_FvZUcHOR2paKoOKhRaULg",
-  authDomain: "openlearn-mm.firebaseapp.com",
-  projectId: "openlearn-mm",
-  storageBucket: "openlearn-mm.firebasestorage.app",
-  messagingSenderId: "977262127138",
-  appId: "1:977262127138:web:0ee1d4ac3c45f1334f427b",
-  measurementId: "G-E65G177ZNJ",
-  databaseURL: "https://openlearn-mm-default-rtdb.firebaseio.com/" // RTDB သုံးမယ်ဆိုရင်
-}; export const LIVE=!!firebaseConfig.apiKey;
+export const firebaseConfig={}; export const LIVE=!!firebaseConfig.apiKey;
+export let auth={}; export let onAuthStateChanged=(a,cb)=>cb(null); export let signOut=async()=>{};
